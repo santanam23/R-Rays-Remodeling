cd20377a92e4f359a67f9158429b15648e016f3a
santanam23 <mirmir11.ms@gmail.com>
2022-01-10 11:49:14 -0600

    adding files to github

---
+++ library/main.js
@@ -0,0 +1,19 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
